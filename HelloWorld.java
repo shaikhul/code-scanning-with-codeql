d30778b8f3941572cfef382e683b79cf4099874d
Shaikhul Islam <shaikhul.net@gmail.com>
2022-05-03 11:36:28 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int a = 0;
+        
+        if (a < 0) {
+          System.out.println("Negative");
+        }
+      
+        System.out.println("Hello World!"); //Display the string.
+    }
+}
